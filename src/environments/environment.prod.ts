c3de056aed661661af9061f4fcf6d2f819fb176e
Usman <usmanbinsaad@vizzweb.com>
2024-03-14 22:49:32 +0500

    changes in Env files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,12 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://localhost:3000/api/',
-  baseUrlData: 'http://localhost:3000/api/'
+  baseUrl: 'http://13.43.7.211:3000/api/',
+  baseUrlData: 'http://13.43.7.211:3000/api/'
 
 };
+// export const environment = {
+//   production: true,
+//   baseUrl: 'http://localhost:3000/api/',
+//   baseUrlData: 'http://localhost:3000/api/'
+
+// };
